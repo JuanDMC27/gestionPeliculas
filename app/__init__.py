5df8dc9d4100fcdd4fc82acb3e9ff55eefc26cc4
JuanDMC27 <juandamcaicedo1027@gmail.com>
2025-07-14 02:44:52 -0500

    Se modifico la configuracion del arranque de la app

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,26 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
-from flask_cors import CORS
 
 db = MongoEngine()
 
 def create_app():
     load_dotenv()
-    app = Flask(__name__)
-    app.secret_key = "misuperclave123456!@#$"
-    CORS(app)
 
+    app = Flask(__name__)
+    CORS(app)
+    app.secret_key = os.getenv("SECRET_KEY", "clave_default_segura")
     app.config['UPLOAD_FOLDER'] = './static/images/'
-    app.config['MONGODB_SETTINGS'] = [{
+    app.config['MONGODB_SETTINGS'] = {
         'db': 'gestionPeliculas',
-        'host': os.environ.get("URI"),
-    }]
+        'host': os.getenv("URI")
+    }
 
     db.init_app(app)
 
-    # Importar y registrar Blueprints
+    # Registro de Blueprints
     from app.routes.genero import ruta_genero
     from app.routes.pelicula import ruta_pelicula
     from app.routes.usuario import ruta_usuario
